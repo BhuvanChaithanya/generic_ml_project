ce7a3dceb087e1f9f40972b21741a01017cf6e07
BhuvanChaithanya <bhuvanchaithanya7@gmail.com>
2025-09-18 23:06:35 +0530

    added notebook and done with some eda

--- src/exception.py
+++ src/exception.py
@@ -1,13 +1,13 @@
 import sys
 import logging
 from src.logger import logging
-def error_message_detail(error, error_detail: sys):
+def error_message_detail(error, error_detail: sys): # type: ignore
     """
     Formats a detailed error message.
     """
     _, _, exc_tb = error_detail.exc_info()
-    file_name = exc_tb.tb_frame.f_code.co_filename
-    line_number = exc_tb.tb_lineno
+    file_name = exc_tb.tb_frame.f_code.co_filename # type: ignore
+    line_number = exc_tb.tb_lineno # type: ignore
     error_message = f"Error occurred in script [{file_name}] at line [{line_number}] with message: [{str(error)}]"
     return error_message
 
@@ -15,7 +15,7 @@ class CustomException(Exception):
     """
     A custom exception class that provides detailed error information.
     """
-    def __init__(self, error_message, error_detail: sys):
+    def __init__(self, error_message, error_detail: sys): # type: ignore
         super().__init__(error_message)
         self.error_message = error_message_detail(error_message, error_detail=error_detail)
 
